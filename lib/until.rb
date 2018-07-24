0d25d7768a5aaf9baf57b9b506969a58b75ff44e
judy2254 <judyjaden225@icloud.com>
2018-07-24 20:00:48 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,10 @@
 def using_until
         levitation_force = 6
-  until beats_in_song==0 do 
+  until levitation_force==0 do 
   puts "One step..."
   puts "Two step..."
   puts "I have #(beats_in_song)steps left." 
-  beats_in_song-=1 # some as beats_in_song= beats_in_song
+  levitation_force-=1 # some as beats_in_song= beats_in_song
 end 
     
 end
